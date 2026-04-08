248885568ac642c7f48159fd312b7e382489180f
polinabreha <polinabreha@gmail.com>
2026-04-08 09:47:09 -0400

    full code

--- src/main/java/com/pluralsight/MathExpressionBuilder.java
+++ src/main/java/com/pluralsight/MathExpressionBuilder.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
 public class MathExpressionBuilder {
+    public static void main(String[] args) {
+        int x = 12;
+        int y = 7;
+        int z = 2;
+        double w = 1.8;
+        String originalExpression = "x + y * z - w";
+        String newExpression = "(x + y )* z - w";
+        System.out.println("Original expression: " + originalExpression);
+        double firstResult = x + y * z - w;
+        System.out.println(firstResult);
+        System.out.println("With parentheses: " + newExpression);
+        double secondResult = (x + y) * z - w;
+        System.out.println(secondResult);
+        double difference = secondResult - firstResult;
+        int roundedDifference = (int) Math.round(difference);
+        System.out.println("The difference between the first and second result is " + roundedDifference);
+
+    }
 }
